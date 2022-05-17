23d70fb9e4e7002938269aea7c23e8e684e3dca1
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-05-17 11:07:48 -0500

    🔨 Add Prototype

---
+++ Patterns/Prototype/Exercise.js
@@ -0,0 +1,31 @@
+/* Given the definitions shown in code, you are asked to
+implement Line.deepCopy() to perform a deep copy of the
+given Line object. This method should return a copy of a
+Line that contains copies of its start/end points.
+*/
+
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Line {
+	constructor(start, end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	deepCopy() {
+		// todo
+		const newStart = new Point(this.start.x, this.start.y);
+		const newEnd = new Point(this.end.x, this.end.y);
+		return new Line(newStart, newEnd);
+	}
+}
+
+module.exports = {
+	Line,
+	Point,
+};
---
+++ Patterns/Prototype/Prototype.md
@@ -0,0 +1,10 @@
+# Prototype
+
+A partially or fully initialized object that you copy (clone) and make use of.
+
+## Summary
+
+- To implement a prototype, partially construct an object and store it somewhere.
+- Deep copy the proptotype.
+- Customize provides a convenient.
+- A factory provides a convenient API for using prototypes.
